fc3c719ec66c00ea959bd469670ca07279d40572
carlysmuir <carlysmuir@ufl.edu>
2020-07-25 15:38:23 +0000

    graphs2

--- Graphs2.R
+++ Graphs2.R
@@ -29,18 +29,18 @@ clean <-mutate(alldata,
   mutate(Intention = str_c(Food.Crops,Biofuels, Both, Other))%>%
   mutate(Intention = str_replace(Intention, "TRUEFALSEFALSEFALSE","Food.crops"))%>%
   mutate(Intention = str_replace(Intention, "FALSETRUEFALSEFALSE","Biofuels"))%>%
-  mutate(Intention = str_replace(Intention, "FALSEFALSETRUEFALSE","Both"))%>%
+  mutate(Intention = str_replace(Intention, "FALSEFALSETRUEFALSE","Food&Biofuels"))%>%
   mutate(Intention = str_replace(Intention, "FALSEFALSEFALSETRUE|FALSEFALSEFALSEFALSE","Other"))
 CleanedData = subset(clean, select = -c(Intention.of.investment, Both, Biofuels,Food.Crops,Other))
+
+selvar <- CleanedData[c(3,4,8,10:13, 21,22),]
 
 #######################################################################################################
 #Summary
 library(ggplot2)
 DealsPerCountry <- (clean %>% group_by(Target.country) %>% count(Target.country, Intention))
 CountCountryType<-as.data.frame(CleanedData %>% count(Intention, Target.country))
-Dealsizes <- as.data.frame(tapply(CleanedData$Deal.size, CleanedData$Target.country, FUN=sum))
-Dealsizes <- Dealsizes/1000
-?tapply
+#Dealsizes <- as.data.frame(tapply(CleanedData$Deal.size, CleanedData$Target.country, FUN=sum))
 DealsPerCountry[is.na(DealsPerCountry)] <-0
 Tidy_Deals <- pivot_wider(DealsPerCountry, 
                           names_from = Intention,
@@ -92,12 +92,13 @@ Totalpc <- (clean %>% count(Target.country))
 #alltran
 dev.off
 
-pdf("LSLTbyIntetion.pdf", ggplot(DealsPerCountry, aes(fill=Intention, y= n, x=Target.country)) + 
+pdf("stacked_transactionbyintention.pdf", ggplot(DealsPerCountry, aes(fill=Intention, y= n, x=Target.country)) + 
   geom_bar(position="stack", stat="identity")+
   theme(axis.text.x = element_text(angle = 60, hjust = 1), plot.title = element_text(
     face = 'bold', hjust = 0.5)) +
   labs(title = "Transaction Intentions", x = "Country", y = "Number of Deals"))
 dev.off()
+
 
 pdf("Facetplot2.pdf", ggplot(DealsPerCountry,
        aes(x = Target.country, y = n)) + 
@@ -108,10 +109,39 @@ pdf("Facetplot2.pdf", ggplot(DealsPerCountry,
   labs(title = "Frequency of Transactions", x = "Country", y = "Number of Deals"))
 dev.off
 
-ggplot(DealsPerCountry,
-       aes(x = Target.country, y = n)) + 
-  geom_bar(stat = 'identity') +
-  facet_wrap(vars(Intention))+
-  theme(axis.text.x = element_text(angle = 60, hjust = 1), plot.title = element_text(
-    face = 'bold', hjust = 0.5)) +
-  labs(title = "Frequency of Transactions", x = "Country", y = "Number of Deals")
+
+
+############################################################################
+Size <- 
+  
+Water <- filter(CleanedData, Water.extraction.envisaged == "Yes")
+waterpts <- points(Water$Longitude, Water$Latitude, col = "red", cex = .4, pch=19) #draw a seq of points at the specified lat/lon
+##Display X,Y Data 
+projection_info<-CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+crs(Africa)<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+water_pt <- SpatialPointsDataFrame(coords = coords, Water, proj4string = projection_info) 
+plot(water_pt)
+########################################################################
+basins <- readOGR(".", "Afr_basins1")
+projection_info<-CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+crs(basins)<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+plot(basins)
+plot(water_pt, col = "blue", cex = .4, pch=19, add = TRUE)
+extobasins <- extract(heat.raster.stack, basins[3,])
+totaltran <- extract(heat.raster.stack, basins, fun = modal)
+
+#what is the lc at the point?
+#first argument is nldc - raster we are using
+#second argument is the point, but we need to use a second argument, st_coordinates
+#this coerices to a numeric matrix, for a single pt it needs to be numeric matrix
+ex<- extract(nlcd, st_coordinates(sesync))
+heat.raster<-lapply(raster.filesALL, raster)
+heat.raster.stack<-stack(heat.raster)
+writeRaster(heat.raster.stack,"stackedcon.tif", )
+#this gives us 23, to find LC class of 23 use indexing
+#use lc descriptive names we created earlier, but remember to add 1 bc indexing starts at 1
+lc_types[sesync_lc + 1]
+
+#summary of raster values from each polygon, what if we wanted modal value at each polygon
+modal_lc <- extract(nlcd_agg,huc_md, fun = modal)
+
